3ae3ddcf9e140d88713e6cb54730edfea06eeec4
aartisab <aartisab@seas.upenn.edu>
2025-04-24 13:03:29 -0400

    Added content.ts content.

--- content.ts
+++ content.ts
@@ -1,5 +1,10 @@
-//file to combine scraping/api calls
+import { extractAllRecipesRecipe, RecipeData } from "./scraper";
 
-import { extractAllRecipesRecipe } from "./scraper";
-
-
+chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
+  if (msg.action === "GET_RECIPE") {
+    const data: RecipeData | null = extractAllRecipesRecipe();
+    sendResponse({ data });
+  }
+  // return true to indicate you’ll respond asynchronously (not strictly needed here)
+  return true;
+});
